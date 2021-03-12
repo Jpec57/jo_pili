aab40b91ea240d0c5e1a65104b7e85b249491adf
jpec57 <jean.paul.bella@hotmail.fr>
2021-03-12 07:49:35 +0100

    Adding text

--- lib/pages/coaching/coaching_page.dart
+++ lib/pages/coaching/coaching_page.dart
@@ -1,10 +1,49 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jo_pili/AppColors.dart';
 import 'package:jo_pili/routes.dart';
 import 'package:jo_pili/widgets/nav_bar.dart';
-import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
+const coaching_service = "Nos séances sont basées sur différents principes :";
+
+const service_title_understand = "Comprendre le mental :";
+const understand_part_1 = "Atteindre un état de fluidité";
+const understand_part_2 = "Découvrir les piliers de la performance mentale";
+
+const service_title_develop = "Développer le mental :";
+const develop_part1 = "Gestion de l’énergie";
+const develop_part2 = "Gestion des émotions";
+const develop_part3 = "Gestion du stress";
+const develop_part4 = "Gestion de l’estime de soi";
+const develop_part5 = "Gestion des objectifs et de la motivation";
+const develop_part6 =
+    "Gestion de la concentration (dans le passé, présent et futur)";
+const develop_part7 = "Gestion de la communication au sein d’un groupe";
+
+const service_title_train = "Entrainer le mental :";
+const train_part1 = "Relâchement";
+const train_part2 = "Imagerie mentale";
+const train_part3 = "Entrainer le mental :";
+const train_part4 = "Réoxygénation de l’estime de soi";
+const train_part5 = "Auto détermination";
+const train_part6 = "Atteindre l’autonomie";
+
+const target = """
+A qui s’adresse le coaching mental ?
+Aux sportifs de tous niveaux
+
+Aux entraineurs
+
+Aux entreprises
+
+Aux entrepreneurs
+
+Aux particuliers
+
+Aux étudiants
+""";
 
 //Reportage france 3 FLORA
 //https://youtu.be/JMRXn3i27d0
@@ -18,20 +57,8 @@ class CoachingPage extends StatefulWidget {
 }
 
 class _CoachingPageState extends State<CoachingPage> {
-  late YoutubePlayerController _controller;
-
   @override
   void initState() {
-    _controller = YoutubePlayerController(
-      initialVideoId: 'JMRXn3i27d0',
-      params: YoutubePlayerParams(
-        playlist: ['nPt8bK2gbaU', 'gQDByCdjUXw'], // Defining custom playlist
-        startAt: Duration(seconds: 30),
-        showControls: true,
-        showFullscreenButton: true,
-      ),
-    );
-
     super.initState();
   }
 
@@ -109,13 +136,24 @@ class _CoachingPageState extends State<CoachingPage> {
               color: AppColors.backgroundColor,
               child: SingleChildScrollView(
                 child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    generateBlurredImage(),
-                    YoutubePlayerIFrame(
-                      controller: _controller,
-                      aspectRatio: 16 / 9,
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16),
+                      child: generateBlurredImage(),
                     ),
+                    Column(
+                      children: [Text("A")],
+                    ),
+                    Container(
+                        height: 200,
+                        color: Colors.red,
+                        child: Image.asset(
+                          "assets/images/mental_pro.jpg",
+                          fit: BoxFit.none,
+                        )),
+                    Image.asset("assets/images/republicain.jpg"),
                   ],
                 ),
               ),
--- lib/pages/sophrologie/sophro_page.dart
+++ lib/pages/sophrologie/sophro_page.dart
@@ -3,63 +3,6 @@ import 'package:jo_pili/AppColors.dart';
 import 'package:jo_pili/routes.dart';
 import 'package:jo_pili/widgets/nav_bar.dart';
 
-const toto = """
-
-SERVICES
-Nos séances sont basées sur différents principes :
-
-Comprendre le mental :
-
-Atteindre un état de fluidité
-
-Découvrir les piliers de la performance mentale
-
-Développer le mental :
-
-Gestion de l’énergie
-
-Gestion des émotions
-
-Gestion du stress
-
-Gestion de l’estime de soi
-
-Gestion des objectifs et de la motivation
-
-gallery/blog-mental-health-638x425
-Gestion de la concentration (dans le passé, présent et futur)
-
-Gestion de la communication au sein d’un groupe
-
-Entrainer le mental :
-
-Relâchement
-
-Imagerie mentale
-
-Réoxygénation de l’estime de soi
-Auto détermination
-
-
-
-Atteindre l’autonomie
-""";
-
-const target = """
-A qui s’adresse le coaching mental ?
-Aux sportifs de tous niveaux
-
-Aux entraineurs
-
-Aux entreprises
-
-Aux entrepreneurs
-
-Aux particuliers
-
-Aux étudiants
-""";
-
 class SophroPage extends StatelessWidget {
   const SophroPage({Key? key}) : super(key: key);
 
